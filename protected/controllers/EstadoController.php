1b1161e957909b4df8a4703ade1b5b738d835a01
Angelica <angelica.informatik@gmail.com>
2017-06-20 12:56:27 -0400

    Cambio de Permisos

--- protected/controllers/EstadoController.php
+++ protected/controllers/EstadoController.php
@@ -28,13 +28,9 @@ class EstadoController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+				'actions'=>array('admin','delete','create','update','view','MunByEst'),
+				'users'=>array('20626929'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
