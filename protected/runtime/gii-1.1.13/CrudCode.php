96ab97cb5de455451257e1ccc06868aa405e5d40
Angelica <angelica.informatik@gmail.com>
2017-06-20 11:13:35 -0400

    Iniciamos

---
+++ protected/runtime/gii-1.1.13/CrudCode.php
@@ -0,0 +1,5 @@
+<?php
+return array (
+  'template' => 'tParametricas',
+  'baseControllerClass' => 'Controller',
+);
---
+++ protected/views/estado/update.php
@@ -0,0 +1,18 @@
+<?php
+/* @var $this EstadoController */
+/* @var $model Estado */
+
+$this->breadcrumbs=array(
+	'Estados'=>array('admin'),
+	'Update',
+);
+
+$this->menu=array(
+	array('label'=>'Create Estado', 'url'=>array('create')),
+	array('label'=>'Manage Estado', 'url'=>array('admin')),
+);
+?>
+
+<h1>Actualizar Estado <?php echo $model->idEstado; ?></h1>
+
+<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
